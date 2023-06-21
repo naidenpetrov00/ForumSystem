bc334d73f1ec348e5548cdbf6e62e97e4d085a6b
Naiden Nikolaev Petrov <naiden.petrov.31.12.00@gmail.com>
2023-06-21 19:30:34 +0300

    Remove logic from View

--- src/Web/ForumSystem.Web.ViewModels/Posts/PostCreateInputModel.cs
+++ src/Web/ForumSystem.Web.ViewModels/Posts/PostCreateInputModel.cs
@@ -2,7 +2,10 @@
 {
 	using System.Collections.Generic;
 	using System.ComponentModel.DataAnnotations;
+	using System.Linq;
+
 	using ForumSystem.Web.ViewModels.Categories;
+	using Microsoft.AspNetCore.Mvc.Rendering;
 
 	public class PostCreateInputModel
 	{
@@ -17,5 +20,7 @@
 		public int CategoryId { get; set; }
 
 		public IEnumerable<CategoryDropDownViewModel> Categories { get; set; }
+
+		public IEnumerable<SelectListItem> SelectListCategories => this.Categories.Select(x => new SelectListItem(x.Name, x.Id.ToString()));
 	}
 }
