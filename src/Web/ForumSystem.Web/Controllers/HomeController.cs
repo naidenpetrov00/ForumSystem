8e1e2e0fcdc972f3187b39aea155cf33e362136a
Naiden Nikolaev Petrov <naiden.petrov.31.12.00@gmail.com>
2023-07-11 13:19:12 +0300

    Update HomeController.cs

--- src/Web/ForumSystem.Web/Controllers/HomeController.cs
+++ src/Web/ForumSystem.Web/Controllers/HomeController.cs
@@ -6,18 +6,25 @@
 	using ForumSystem.Web.ViewModels;
 	using ForumSystem.Web.ViewModels.Home;
 	using Microsoft.AspNetCore.Mvc;
+	using Microsoft.Extensions.Logging;
 
 	public class HomeController : BaseController
 	{
 		private readonly ICategoriesService categoriesService;
+		private readonly ILogger<HomeController> logger;
 
-		public HomeController(ICategoriesService categoriesService)
+		public HomeController(
+			ICategoriesService categoriesService,
+			ILogger<HomeController> logger)
 		{
 			this.categoriesService = categoriesService;
+			this.logger = logger;
 		}
 
 		public IActionResult Index()
 		{
+			this.logger.LogWarning(5, "Log Test");
+
 			var viewModel = new IndexViewModel
 			{
 				Categories = this.categoriesService.GetAll<IndexCategoryViewModel>(),
