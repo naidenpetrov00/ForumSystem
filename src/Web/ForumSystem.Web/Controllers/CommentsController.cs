9a48c2b41cad44a58cb8d72d1bad1120b540bb25
Naiden Nikolaev Petrov <naiden.petrov.31.12.00@gmail.com>
2023-07-21 21:20:38 +0300

    Add the AJAX Call
    Took me 2 hours  niceeeeeeeeee

--- src/Web/ForumSystem.Web/Controllers/CommentsController.cs
+++ src/Web/ForumSystem.Web/Controllers/CommentsController.cs
@@ -1,43 +1,44 @@
 ﻿namespace ForumSystem.Web.Controllers
 {
-    using System.Threading.Tasks;
+	using System.Threading.Tasks;
 
-    using ForumSystem.Data.Models;
-    using ForumSystem.Services.Data.Interfaces;
-    using ForumSystem.Web.ViewModels.Comments;
-    using Microsoft.AspNetCore.Identity;
-    using Microsoft.AspNetCore.Mvc;
+	using ForumSystem.Data.Models;
+	using ForumSystem.Services.Data.Interfaces;
+	using ForumSystem.Web.ViewModels.Comments;
+	using Microsoft.AspNetCore.Identity;
+	using Microsoft.AspNetCore.Mvc;
+	using Newtonsoft.Json;
 
-    public class CommentsController : Controller
-    {
-        private readonly ICommentService commentService;
-        private readonly UserManager<ApplicationUser> userManager;
+	public class CommentsController : Controller
+	{
+		private readonly ICommentService commentService;
+		private readonly UserManager<ApplicationUser> userManager;
 
-        public CommentsController(
-            ICommentService commentService,
-            UserManager<ApplicationUser> userManager)
-        {
-            this.commentService = commentService;
-            this.userManager = userManager;
-        }
+		public CommentsController(
+			ICommentService commentService,
+			UserManager<ApplicationUser> userManager)
+		{
+			this.commentService = commentService;
+			this.userManager = userManager;
+		}
 
-        [HttpPost]
-        public async Task<IActionResult> Create(CreateCommentInputModel input)
-        {
-            var parentCommentId = input.ParentCommentId == 0 ? (int?)null : input.ParentCommentId;
+		[HttpPost]
+        public async Task<IActionResult> Create([FromBody] CreateCommentInputModel input)
+		{
+			var parentCommentId = input.ParentCommentId == 0 ? (int?)null : input.ParentCommentId;
 
-            if (parentCommentId.HasValue)
-            {
-                if (!this.commentService.IsInPostId(parentCommentId.Value, input.PostId))
-                {
-                    return this.BadRequest();
-                }
-            }
+			if (parentCommentId.HasValue)
+			{
+				if (!this.commentService.IsInPostId(parentCommentId.Value, input.PostId))
+				{
+					return this.BadRequest();
+				}
+			}
 
-            var userId = this.userManager.GetUserId(this.User);
-            await this.commentService.Create(input.PostId, userId, input.Content, parentCommentId);
+			var userId = this.userManager.GetUserId(this.User);
+			await this.commentService.Create(input.PostId, userId, input.Content, parentCommentId);
 
-            return this.RedirectToAction("ById", "Posts", new { id = input.PostId });
-        }
-    }
+			return this.Ok();
+		}
+	}
 }
