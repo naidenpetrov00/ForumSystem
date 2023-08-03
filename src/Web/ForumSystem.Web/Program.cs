72b7dd207c6afc4350c198780c3c83d9f309c29f
Naiden Nikolaev Petrov <naiden.petrov.31.12.00@gmail.com>
2023-08-03 19:46:16 +0300

    Add HangFire

--- src/Web/ForumSystem.Web/Program.cs
+++ src/Web/ForumSystem.Web/Program.cs
@@ -1,168 +1,180 @@
 ﻿namespace ForumSystem.Web
 {
-    using System;
-    using System.Reflection;
+	using System;
+	using System.Reflection;
 
-    using ForumSystem.Common.Models;
-    using ForumSystem.Data;
-    using ForumSystem.Data.Common;
-    using ForumSystem.Data.Common.Repositories;
-    using ForumSystem.Data.Models;
-    using ForumSystem.Data.Repositories;
-    using ForumSystem.Data.Seeding;
-    using ForumSystem.Services.Data;
-    using ForumSystem.Services.Data.Interfaces;
-    using ForumSystem.Services.Mapping;
-    using ForumSystem.Services.Messaging;
-    using ForumSystem.Web.ViewModels;
-    using Microsoft.AspNetCore.Builder;
-    using Microsoft.AspNetCore.Http;
-    using Microsoft.AspNetCore.Mvc;
-    using Microsoft.EntityFrameworkCore;
-    using Microsoft.Extensions.Configuration;
-    using Microsoft.Extensions.DependencyInjection;
-    using Microsoft.Extensions.Hosting;
+	using ForumSystem.Common.Models;
+	using ForumSystem.Data;
+	using ForumSystem.Data.Common;
+	using ForumSystem.Data.Common.Repositories;
+	using ForumSystem.Data.Models;
+	using ForumSystem.Data.Repositories;
+	using ForumSystem.Data.Seeding;
+	using ForumSystem.Services.Data;
+	using ForumSystem.Services.Data.Interfaces;
+	using ForumSystem.Services.Mapping;
+	using ForumSystem.Services.Messaging;
+	using ForumSystem.Web.ViewModels;
+	using Hangfire;
+	using Microsoft.AspNetCore.Builder;
+	using Microsoft.AspNetCore.Http;
+	using Microsoft.AspNetCore.Mvc;
+	using Microsoft.EntityFrameworkCore;
+	using Microsoft.Extensions.Configuration;
+	using Microsoft.Extensions.DependencyInjection;
+	using Microsoft.Extensions.Hosting;
 
-    public class Program
-    {
-        public static void Main(string[] args)
-        {
-            var builder = WebApplication.CreateBuilder(args);
-            ConfigureServices(builder.Services, builder.Configuration);
-            var app = builder.Build();
-            Configure(app);
-            app.Run();
-        }
+	public class Program
+	{
+		public static void Main(string[] args)
+		{
+			var builder = WebApplication.CreateBuilder(args);
+			ConfigureServices(builder.Services, builder.Configuration);
+			var app = builder.Build();
+			Configure(app);
+			app.Run();
+		}
 
-        private static void ConfigureServices(IServiceCollection services, IConfiguration configuration)
-        {
-            services.AddDbContext<ApplicationDbContext>(
-                options => options.UseSqlServer(configuration.GetConnectionString("DefaultConnection")));
+		private static void ConfigureServices(IServiceCollection services, IConfiguration configuration)
+		{
+			services.AddDbContext<ApplicationDbContext>(
+				options => options.UseSqlServer(configuration.GetConnectionString("DefaultConnection")));
 
-            services.AddDefaultIdentity<ApplicationUser>(IdentityOptionsProvider.GetIdentityOptions)
-                .AddRoles<ApplicationRole>().AddEntityFrameworkStores<ApplicationDbContext>();
+			services.AddDefaultIdentity<ApplicationUser>(IdentityOptionsProvider.GetIdentityOptions)
+				.AddRoles<ApplicationRole>().AddEntityFrameworkStores<ApplicationDbContext>();
 
-            services.Configure<CookiePolicyOptions>(
-                options =>
-                {
-                    options.CheckConsentNeeded = context => true;
-                    options.MinimumSameSitePolicy = SameSiteMode.None;
-                });
+			services.Configure<CookiePolicyOptions>(
+				options =>
+				{
+					options.CheckConsentNeeded = context => true;
+					options.MinimumSameSitePolicy = SameSiteMode.None;
+				});
 
-            services.AddControllersWithViews(
-                options =>
-                {
-                    options.Filters.Add(new AutoValidateAntiforgeryTokenAttribute());
-                })
-                .AddRazorRuntimeCompilation();
-            services.AddAutoMapper(typeof(AutoMapperConfig));
-            services.AddAntiforgery(options =>
-            {
-                options.HeaderName = "X-CSRF-TOKEN";
-            });
-            services.AddSwaggerGen();
+			services.AddControllersWithViews(
+				options =>
+				{
+					options.Filters.Add(new AutoValidateAntiforgeryTokenAttribute());
+				})
+				.AddRazorRuntimeCompilation();
+			services.AddAutoMapper(typeof(AutoMapperConfig));
+			services.AddAntiforgery(options =>
+			{
+				options.HeaderName = "X-CSRF-TOKEN";
+			});
+			services.AddSwaggerGen();
 
-            services.AddRazorPages();
-            services.AddDatabaseDeveloperPageExceptionFilter();
+			services.AddRazorPages();
+			services.AddDatabaseDeveloperPageExceptionFilter();
 
-            services.AddSingleton(configuration);
+			services.AddSingleton(configuration);
 
-            services.AddDistributedSqlServerCache(options =>
-            {
-                options.ConnectionString = configuration.GetConnectionString("DefaultConnection");
-                options.SchemaName = "dbo";
-                options.TableName = "CacheRecords";
-            });
-            services.AddSession(options =>
-            {
-                options.IdleTimeout = TimeSpan.FromDays(2);
-                options.Cookie.HttpOnly = true;
-                options.Cookie.IsEssential = true;
-            });
-            services.AddResponseCompression(options =>
-            {
-                options.EnableForHttps = true;
-            });
+			services.AddDistributedSqlServerCache(options =>
+			{
+				options.ConnectionString = configuration.GetConnectionString("DefaultConnection");
+				options.SchemaName = "dbo";
+				options.TableName = "CacheRecords";
+			});
+			services.AddSession(options =>
+			{
+				options.IdleTimeout = TimeSpan.FromDays(2);
+				options.Cookie.HttpOnly = true;
+				options.Cookie.IsEssential = true;
+			});
+			services.AddResponseCompression(options =>
+			{
+				options.EnableForHttps = true;
+			});
 
-            // Data repositories
-            services.AddScoped(typeof(IDeletableEntityRepository<>), typeof(EfDeletableEntityRepository<>));
-            services.AddScoped(typeof(IRepository<>), typeof(EfRepository<>));
-            services.AddScoped<IDbQueryRunner, DbQueryRunner>();
+			// Hangfire
+			services.AddHangfire(options => options
+			 .SetDataCompatibilityLevel(CompatibilityLevel.Version_180)
+			 .UseSimpleAssemblyNameTypeSerializer()
+			 .UseRecommendedSerializerSettings()
+			 .UseSqlServerStorage(configuration.GetConnectionString("DefaultConnection")));
+			services.AddHangfireServer();
 
-            // Application services
-            services.AddTransient<IEmailSender, NullMessageSender>();
-            services.AddTransient<ISettingsService, SettingsService>();
-            services.AddTransient<ICategoriesService, CategoriesService>();
-            services.AddTransient<IPostService, PostService>();
-            services.AddTransient<IVoteService, VotesService>();
-            services.AddTransient<ICommentService, CommentService>();
-        }
+			// Data repositories
+			services.AddScoped(typeof(IDeletableEntityRepository<>), typeof(EfDeletableEntityRepository<>));
+			services.AddScoped(typeof(IRepository<>), typeof(EfRepository<>));
+			services.AddScoped<IDbQueryRunner, DbQueryRunner>();
 
-        private static void Configure(WebApplication app)
-        {
-            // Seed data on application startup
-            using (var serviceScope = app.Services.CreateScope())
-            {
-                var dbContext = serviceScope.ServiceProvider.GetRequiredService<ApplicationDbContext>();
-                dbContext.Database.Migrate();
-                new ApplicationDbContextSeeder().SeedAsync(dbContext, serviceScope.ServiceProvider).GetAwaiter().GetResult();
-            }
+			// Application services
+			services.AddTransient<IEmailSender, NullMessageSender>();
+			services.AddTransient<ISettingsService, SettingsService>();
+			services.AddTransient<ICategoriesService, CategoriesService>();
+			services.AddTransient<IPostService, PostService>();
+			services.AddTransient<IVoteService, VotesService>();
+			services.AddTransient<ICommentService, CommentService>();
+		}
 
-            AutoMapperConfig.RegisterMappings(typeof(ErrorViewModel).GetTypeInfo().Assembly);
+		private static void Configure(WebApplication app)
+		{
+			// Seed data on application startup
+			using (var serviceScope = app.Services.CreateScope())
+			{
+				var dbContext = serviceScope.ServiceProvider.GetRequiredService<ApplicationDbContext>();
+				dbContext.Database.Migrate();
+				new ApplicationDbContextSeeder().SeedAsync(dbContext, serviceScope.ServiceProvider).GetAwaiter().GetResult();
+			}
 
-            app.UseSwagger();
-            app.UseSwaggerUI();
+			AutoMapperConfig.RegisterMappings(typeof(ErrorViewModel).GetTypeInfo().Assembly);
 
-            if (app.Environment.IsDevelopment())
-            {
-                app.UseDeveloperExceptionPage();
-                app.UseMigrationsEndPoint();
-            }
-            else
-            {
-                app.UseExceptionHandler("/Home/Error");
-                app.UseHsts();
-            }
+			app.UseSwagger();
+			app.UseSwaggerUI();
 
-            app.Use((context, next) =>
-            {
-                if (!context.Request.Cookies.ContainsKey(GuestCookieModel.Key))
-                {
-                    var cookieOptions = new CookieOptions()
-                    {
-                        Expires = GuestCookieModel.Expires,
-                        IsEssential = true,
-                        SameSite = SameSiteMode.Strict,
-                    };
+			if (app.Environment.IsDevelopment())
+			{
+				app.UseDeveloperExceptionPage();
+				app.UseMigrationsEndPoint();
+			}
+			else
+			{
+				app.UseExceptionHandler("/Home/Error");
+				app.UseHsts();
+			}
 
-                    context.Response.Cookies.Append(GuestCookieModel.Key, GuestCookieModel.Value, cookieOptions);
-                }
+			app.Use((context, next) =>
+			{
+				if (!context.Request.Cookies.ContainsKey(GuestCookieModel.Key))
+				{
+					var cookieOptions = new CookieOptions()
+					{
+						Expires = GuestCookieModel.Expires,
+						IsEssential = true,
+						SameSite = SameSiteMode.Strict,
+					};
 
-                return next();
-            });
-            app.UseResponseCompression();
-            app.UseSession();
-            app.UseHttpsRedirection();
-            app.UseStaticFiles();
-            app.UseCookiePolicy();
+					context.Response.Cookies.Append(GuestCookieModel.Key, GuestCookieModel.Value, cookieOptions);
+				}
 
-            app.UseRouting();
+				return next();
+			});
+			app.UseResponseCompression();
+			app.UseSession();
+			app.UseHttpsRedirection();
+			app.UseStaticFiles();
+			app.UseCookiePolicy();
 
-            app.UseAuthentication();
-            app.UseAuthorization();
+			app.UseRouting();
 
-            app.MapControllerRoute(
-                "areaRoute",
-                "{area:exists}/{controller=Home}/{action=Index}/{id?}");
-            app.MapControllerRoute(
-                "default",
-                "{controller=Home}/{action=Index}/{id?}");
-            app.MapControllerRoute(
-                "forumCategory",
-                "f/{name:minlength(3)}",
-                new { controller = "Categories", action = "ByName" });
+			app.UseAuthentication();
+			app.UseAuthorization();
 
-            app.MapRazorPages();
-        }
-    }
+			app.UseHangfireDashboard();
+
+			app.MapHangfireDashboard();
+			app.MapControllerRoute(
+				"areaRoute",
+				"{area:exists}/{controller=Home}/{action=Index}/{id?}");
+			app.MapControllerRoute(
+				"default",
+				"{controller=Home}/{action=Index}/{id?}");
+			app.MapControllerRoute(
+				"forumCategory",
+				"f/{name:minlength(3)}",
+				new { controller = "Categories", action = "ByName" });
+
+			app.MapRazorPages();
+		}
+	}
 }
